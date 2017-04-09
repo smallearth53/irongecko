d8995101a30453172609e0bece037615d4b983b9
smallearth53 <vagrant@homestead>
2017-04-09 18:44:59 +0000

    503 page modified

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <link rel="icon" type="image/x-icon" href="{{asset('img/favicon.ico')}}">
 
         <title>Site Under Construction</title>
@@ -12,6 +12,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -46,25 +47,85 @@
                 font-size: 64px;
                 padding-bottom: 50px;
             }
+            
+            
+            
+
+            @media(max-width:767px){
+                /*                .title {
+                                    
+                                    font-size: 2.2em;
+                                }*/
+
+                .title {
+                    font-size: 2em;
+                    font-weight: bold;
+                    padding-left: 2px;
+                    padding-right: 2px;
+                    padding-bottom: 0px;
+                    margin-bottom: 0px;
+                    opacity: 0.7;
+                }
+                
+                #image {
+                    width: 300px;
+                }
+                                #content {
+                position: fixed;
+                top: 50%;
+                left: 50%;
+                /* bring your own prefixes */
+                transform: translate(-50%, -50%);
+            }
+            }
+            @media(min-width:768px){
+                                 #content {
+                position: fixed;
+                top: 50%;
+                left: 50%;
+                /* bring your own prefixes */
+                transform: translate(-50%, -50%);
+            }
+            }
+            @media(min-width:992px){
+
+            }
+            @media(min-width:1200px){
+
+            }
+
+           
+
+
+
         </style>
     </head>
     <body>
-        
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                
-                   
-                <div class="title">
-                    Site Under Construction
-                    
-                </div>
-              
-                <div>
-                    <img src="{{asset('img/coffee-cup.png')}}">
+
+        <div class="container">
+            <div class="row text-center">
+
+
+
+                <div id='content'>
+                    <div>
+                        <img id='image' src="{{asset('img/skyline.png')}}">
                     </div>
+
+                    <div class="title">
+                        Irongecko Productions<br/>
+                        <p class='subtext' style='font-size: 0.5em;'>Will soon be in Town<p>
+
+                    </div>
+                </div>
+
+
+
+
+
             </div>
         </div>
-           
+
     </body>
 </html>
 
