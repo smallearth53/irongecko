156c2feff5baba67a85ba81f73783c7f51bfb2f9
smallearth53 <vagrant@homestead>
2017-04-10 15:07:57 +0000

    stable version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,7 @@
 
                 </div>
                 <div class='col-3'>
-                    <img id='logo' class='img-fluid text-center wow fadeIn' src='/img/gecko.png' width='250px'/>
+                    <img id='logo' class='img-fluid text-center wow fadeIn' data-wow-delay="0.1s" src='/img/gecko.png' width='250px'/>
                 </div>
                 
             </div>
@@ -288,6 +288,153 @@
 
         </section>
         <!--/Section: Testimonials v.3-->
+
+<!--Section: Pricing v.1-->
+<section class="section">
+
+    <!--Section heading-->
+    <h1 class="section-heading">Our pricing plans v.1</h1>
+    <!--Section description-->
+    <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur accusamus veniam. Quia, minima?</p>
+
+    <!--First row-->
+    <div class="row">
+
+        <!--First column-->
+        <div class="col-lg-4 col-md-12 mb-r">
+
+            <!--Pricing card-->
+            <div class="card pricing-card">
+                <!--Price-->
+                <div class="price header blue">
+                    <h1>10</h1>
+                    <div class="version">
+                        <h5>Basic</h5>
+                    </div>
+                </div>
+                <!--/.Price-->
+
+                <!--Features-->
+                <div class="card-block striped">
+                    <ul>
+                        <li>
+                            <p><i class="fa fa-check"></i> 20 GB Of Storage</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> 2 Email Accounts</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-times"></i> 24h Tech Support</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-times"></i> 300 GB Bandwidth</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-times"></i> User Management </p>
+                        </li>
+                    </ul>
+
+                    <button class="btn btn-primary">Buy now</button>
+                </div>
+                <!--/.Features-->
+
+            </div>
+            <!--/.Pricing card-->
+
+        </div>
+        <!--/First column-->
+
+        <!--Second column-->
+        <div class="col-lg-4 col-md-6 mb-r">
+
+            <!--Pricing card-->
+            <div class="card pricing-card">
+                <!--Price-->
+                <div class="price header indigo">
+                    <h1>20</h1>
+                    <div class="version">
+                        <h5>Pro</h5>
+                    </div>
+                </div>
+                <!--/.Price-->
+
+                <!--Features-->
+                <div class="card-block striped">
+                    <ul>
+                        <li>
+                            <p><i class="fa fa-check"></i> 20 GB Of Storage</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> 4 Email Accounts</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> 24h Tech Support</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-times"></i> 300 GB Bandwidth</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-times"></i> User Management </p>
+                        </li>
+                    </ul>
+
+                    <button class="btn btn-primary">Buy now</button>
+                </div>
+                <!--/.Features-->
+
+            </div>
+            <!--/.Pricing card-->
+
+        </div>
+        <!--/Second column-->
+
+        <!--Third column-->
+        <div class="col-lg-4 col-md-6 mb-r">
+            <!--Pricing card-->
+            <div class="card pricing-card">
+                <!--Price-->
+                <div class="price header deep-purple">
+                    <h1>30</h1>
+                    <div class="version">
+                        <h5>Enterprise</h5>
+                    </div>
+                </div>
+                <!--/.Price-->
+
+                <!--Features-->
+                <div class="card-block striped">
+                    <ul>
+                        <li>
+                            <p><i class="fa fa-check"></i> 30 GB Of Storage</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> 5 Email Accounts</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> 24h Tech Support</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> 300 GB Bandwidth</p>
+                        </li>
+                        <li>
+                            <p><i class="fa fa-check"></i> User Management </p>
+                        </li>
+                    </ul>
+
+                    <button class="btn btn-primary">Buy now</button>
+                </div>
+                <!--/.Features-->
+
+            </div>
+            <!--/.Pricing card-->
+        </div>
+        <!--/Third column-->
+
+    </div>
+    <!--/First row-->
+
+</section>
+<!--/Section: Pricing v.1-->
 
 
 
